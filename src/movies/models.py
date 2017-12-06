cde9b3073e98b59b508a84be93de602c0eb404f2
Yosnier Samon M <yosamac@gmail.com>
2017-12-06 03:39:57 +0100

    Ignoring sqlite Database and making migrations

--- src/movies/models.py
+++ src/movies/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+class Category(models.Model):
+    name = models.CharField(max_length=50)
+    description = models.TextField(blank=True, null=True) # Optional
+
+
+class Movie(models.Model):
+    title = models.CharField(max_length=150)
+    summary = models.TextField()
+    director_name = models.CharField(max_length=100)
+    realease_date = models.DateField()
+    image = models.URLField()
+    rating = models.FloatField()
+    created_at = models.DateTimeField(auto_now_add=True) # save the date when the object is created
+    modified_at = models.DateTimeField(auto_now_add=True) # save the date when the object is updated
