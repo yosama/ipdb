04d3acbd1479e767e037ce5b0ee240db95fe0a37
Yosnier Samon M <yosamac@gmail.com>
2017-12-10 21:09:27 +0100

    Registered the models in the Django admin

--- src/movies/admin.py
+++ src/movies/admin.py
@@ -1,3 +1,7 @@
+
 from django.contrib import admin
 
-# Register your models here.
+from movies.models import Category, Movie
+
+admin.site.register(Category)
+admin.site.register(Movie)
--- src/movies/models.py
+++ src/movies/models.py
@@ -1,8 +1,13 @@
 from django.db import models
 
+
 class Category(models.Model):
     name = models.CharField(max_length=50)
     description = models.TextField(blank=True, null=True) # Optional
+
+    def __str__(self):
+        return self.name
+
 
 
 class Movie(models.Model):
@@ -14,3 +19,6 @@ class Movie(models.Model):
     rating = models.FloatField()
     created_at = models.DateTimeField(auto_now_add=True) # save the date when the object is created
     modified_at = models.DateTimeField(auto_now_add=True) # save the date when the object is updated
+
+    def __str__(self):
+        return self.title
