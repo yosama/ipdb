7eda7a0f554ea3b70e40320ff87ca69dd1a01e84
Yosnier Samon M <yosamac@gmail.com>
2017-12-15 17:51:17 +0100

    Added movies list and creation endpoint using REST framework generics view

--- src/movies/api.py
+++ src/movies/api.py
@@ -1,3 +1,4 @@
+from rest_framework.generics import ListCreateAPIView
 from rest_framework.views import APIView
 from rest_framework.response import Response
 
@@ -5,9 +6,7 @@ from movies.models import Movie
 from movies.serializers import MovieSerializer
 
 
-class MovieListAPI(APIView):
+class MovieListAPI(ListCreateAPIView):
 
-    def get(self, request):
-        movies = Movie.objects.all()
-        serializer = MovieSerializer(movies, many=True)
-        return  Response(serializer.data)
+    queryset = Movie.objects.all()
+    serializer_class = MovieSerializer
