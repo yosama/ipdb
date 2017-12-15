cd33ebe9334a94f8b7de20fa54a0ad232bba568b
Yosnier Samon M <yosamac@gmail.com>
2017-12-15 18:40:57 +0100

    Added the pagination in the movies and users results

--- src/ipdb/settings.py
+++ src/ipdb/settings.py
@@ -147,3 +147,9 @@ LOGGING = {
 
 # Login URL
 LOGIN_URL = "/login"
+
+# Django REST framework  settings
+REST_FRAMEWORK = {
+    "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.PageNumberPagination",
+    'PAGE_SIZE': 2
+}
--- src/users/api.py
+++ src/users/api.py
@@ -1,5 +1,6 @@
 from django.contrib.auth.models import User
 from rest_framework import status
+from rest_framework.pagination import PageNumberPagination
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework.generics import get_object_or_404
@@ -22,8 +23,11 @@ class UsersListAPI(APIView):
 
     def get(self, request):
         users = User.objects.all()
-        serializer = UsersListSerializer(users, many=True)
-        return Response(serializer.data)
+        paginator = PageNumberPagination()
+        # paginate queryset
+        paginate_users = paginator.paginate_queryset(users, request)
+        serializer = UsersListSerializer(paginate_users, many=True)
+        return paginator.get_paginated_response(serializer.data)
 
     def post(self, request):
         serializer = UserSerializer(data=request.data)
