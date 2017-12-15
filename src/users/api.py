aee0f8bf6dcdabac50adaa0b569d7237eed6d242
Yosnier Samon M <yosamac@gmail.com>
2017-12-15 17:27:20 +0100

    Added the user delete endpoint

--- src/users/api.py
+++ src/users/api.py
@@ -49,3 +49,8 @@ class UserDetailAPI(APIView):
             return Response(serializer.data, status=status.HTTP_202_ACCEPTED)
         else:
             return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def delete(self, request, pk):
+        user = get_object_or_404(User, pk=pk)
+        user.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
