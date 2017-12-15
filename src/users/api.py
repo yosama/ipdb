01dffcc5e45ac96ca67defa0b4a3f8d16fc9d80d
Yosnier Samon M <yosamac@gmail.com>
2017-12-15 14:45:08 +0100

    Added the user creation endpoint

--- src/users/api.py
+++ src/users/api.py
@@ -1,4 +1,5 @@
 from django.contrib.auth.models import User
+from rest_framework import status
 from rest_framework.views import APIView
 from rest_framework.response import Response
 
@@ -22,3 +23,11 @@ class UsersListAPI(APIView):
         users = User.objects.all()
         serializer = UserSerializer(users, many=True)
         return Response(serializer.data)
+
+    def post(self, request):
+        serializer = UserSerializer(data=request.data)
+        if serializer.is_valid():
+            user = serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        else:
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
--- src/users/serializers.py
+++ src/users/serializers.py
@@ -1,4 +1,6 @@
+from django.contrib.auth.models import User
 from rest_framework import serializers
+from rest_framework.exceptions import ValidationError
 
 
 class UserSerializer(serializers.Serializer):
@@ -10,8 +12,20 @@ class UserSerializer(serializers.Serializer):
     email = serializers.EmailField()
     password = serializers.CharField()
 
+    def validate_username(self, data):
+        if User.objects.filter(username=data).exists():
+            raise ValidationError("User already exists")
+        return data
+
     def create(self, validated_data):
-        pass
+        instance = User()
+        instance.first_name = validated_data.get("first_name")
+        instance.last_name = validated_data.get("last_name")
+        instance.username = validated_data.get("username")
+        instance.email = validated_data.get("email")
+        instance.set_password(validated_data.get("password"))
+        instance.save()
+        return instance
 
     def update(self, instance, validated_data):
         pass
