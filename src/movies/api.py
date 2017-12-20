cbcca07d45b82afb19652b29d1ac795b91ef0864
Yosnier Samon M <yosamac@gmail.com>
2017-12-20 02:09:17 +0100

    Added search, ordering and filter

--- src/movies/api.py
+++ src/movies/api.py
@@ -1,3 +1,5 @@
+from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework.filters import SearchFilter, OrderingFilter
 from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView
 from rest_framework.permissions import IsAuthenticatedOrReadOnly
 from rest_framework.views import APIView
@@ -13,6 +15,10 @@ class MovieListAPI(ListCreateAPIView):
 
     queryset = Movie.objects.all()
     permission_classes = [IsAuthenticatedOrReadOnly]
+    filter_backends = [SearchFilter, OrderingFilter, DjangoFilterBackend]
+    search_fields = ["title", "director_name", "category__name", "summary"]
+    ordering_fields = ["title", "release_date", "created_at", "modified_at", "rating", "director_name", "category"]
+    filter_fields = ["user", "category", "release_date"]
 
     def get_serializer_class(self):
         return MoviesListSerializer if self.request.method == "GET" else MovieSerializer
@@ -32,6 +38,7 @@ class MovieDetailAPI(RetrieveUpdateDestroyAPIView):
 
 
 class CategoryViewSet(ModelViewSet):
+
     queryset = Category.objects.all()
-    serializer_class = CategorySerializer
+    serializers_class = CategorySerializer
     permission_classes = [CategoriesPermission]
